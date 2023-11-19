984e626ca2460b07b0225902974650b2ee4a90a4
Francys Rodrigues <blackgsgofsr@gmail.com>
2023-11-19 18:27:04 -0300

    Changes to be committed:
    	modified:   minesweeper/minesweeper.py

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -2,147 +2,130 @@ import itertools
 import random
 import copy
 
-
 class Minesweeper():
     """
+    Representação do jogo Campo Minado
     Minesweeper game representation
     """
 
-    def __init__(self, height=8, width=8, mines=8):
-
+    def __init__(self, height=16, width=16, mines=12):
+        # Define a largura, altura e número de minas iniciais
         # Set initial width, height, and number of mines
         self.height = height
         self.width = width
         self.mines = set()
 
+        # Inicializa um campo vazio sem minas
         # Initialize an empty field with no mines
-        self.board = []
-        for i in range(self.height):
-            row = []
-            for j in range(self.width):
-                row.append(False)
-            self.board.append(row)
+        self.board = [[False for _ in range(self.width)] for _ in range(self.height)]
 
+        # Adiciona minas aleatoriamente
         # Add mines randomly
         while len(self.mines) != mines:
-            i = random.randrange(height)
-            j = random.randrange(width)
+            i, j = random.randrange(height), random.randrange(width)
             if not self.board[i][j]:
                 self.mines.add((i, j))
                 self.board[i][j] = True
 
+        # No início, o jogador não encontrou nenhuma mina
         # At first, player has found no mines
         self.mines_found = set()
 
     def print(self):
-        """
-        Prints a text-based representation
-        of where mines are located.
-        """
+        # Imprime uma representação baseada em texto de onde as minas estão localizadas
+        # Prints a text-based representation of where mines are located
         for i in range(self.height):
             print("--" * self.width + "-")
             for j in range(self.width):
-                if self.board[i][j]:
-                    print("|X", end="")
-                else:
-                    print("| ", end="")
+                print("|X" if self.board[i][j] else "| ", end="")
             print("|")
         print("--" * self.width + "-")
 
     def is_mine(self, cell):
+        # Retorna se uma célula é uma mina
+        # Returns whether a cell is a mine
         i, j = cell
         return self.board[i][j]
 
     def nearby_mines(self, cell):
-        """
-        Returns the number of mines that are
-        within one row and column of a given cell,
-        not including the cell itself.
-        """
+        # Retorna o número de minas próximas a uma dada célula
+        # Returns the number of mines that are near a given cell
 
-        # Keep count of nearby mines
         count = 0
-
-        # Loop over all cells within one row and column
         for i in range(cell[0] - 1, cell[0] + 2):
             for j in range(cell[1] - 1, cell[1] + 2):
-
-                # Ignore the cell itself
-                if (i, j) == cell:
+                if (i, j) == cell or not (0 <= i < self.height and 0 <= j < self.width):
                     continue
-
-                # Update count if cell in bounds and is mine
-                if 0 <= i < self.height and 0 <= j < self.width:
-                    if self.board[i][j]:
-                        count += 1
+                if self.board[i][j]:
+                    count += 1
 
         return count
 
     def won(self):
-        """
-        Checks if all mines have been flagged.
-        """
+        # Verifica se todas as minas foram marcadas
+        # Checks if all mines have been flagged
         return self.mines_found == self.mines
-
 
 class Sentence():
     """
+    Declaração lógica sobre o jogo Campo Minado
+    Uma sentença consiste em um conjunto de células do tabuleiro,
+    e uma contagem do número dessas células que são minas.
     Logical statement about a Minesweeper game
     A sentence consists of a set of board cells,
     and a count of the number of those cells which are mines.
     """
-
     def __init__(self, cells, count):
+        # Inicializa a sentença com células e contagem de minas
+        # Initializes the sentence with cells and mine count
         self.cells = set(cells)
         self.count = count
 
     def __eq__(self, other):
+        # Verifica a igualdade entre duas sentenças
+        # Checks equality between two sentences
         return self.cells == other.cells and self.count == other.count
 
     def __str__(self):
+        # Retorna uma representação em string da sentença
+        # Returns a string representation of the sentence
         return f"{self.cells} = {self.count}"
 
     def known_mines(self):
-        """
-        Returns the set of all cells in self.cells known to be mines.
-        """
+        # Retorna o conjunto de todas as células conhecidas por serem minas
+        # Returns the set of all cells known to be mines
         if self.count == len(self.cells):
-            return self.cells
+            return copy.deepcopy(self.cells)
+        return set()
 
     def known_safes(self):
-        """
-        Returns the set of all cells in self.cells known to be safe.
-        """
+        # Retorna o conjunto de todas as células conhecidas por serem seguras
+        # Returns the set of all cells known to be safe
         if self.count == 0:
-            return self.cells
+            return copy.deepcopy(self.cells)
+        return set()
 
     def mark_mine(self, cell):
-        """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be a mine.
-        """
+        # Atualiza a representação do conhecimento interno dado que
+        # uma célula é conhecida por ser uma mina
+        # Updates internal knowledge representation given that
+        # a cell is known to be a mine
         if cell in self.cells:
             self.cells.remove(cell)
             self.count -= 1
-        else:
-            pass
 
     def mark_safe(self, cell):
-        """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be safe.
-        """
+        # Atualiza a representação do conhecimento interno dado que
+        # uma célula é conhecida por ser segura
+        # Updates internal knowledge representation given that
+        # a cell is known to be safe
         if cell in self.cells:
             self.cells.remove(cell)
-        else:
-            pass
-
 
 class MinesweeperAI():
     """
     Minesweeper game player
     """
-
     def __init__(self, height=8, width=8):
 
         # Set initial height and width

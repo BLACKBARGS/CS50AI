3f45d792f19a4094d6d28b4216c83b356fbb06e5
Francys Rodrigues <blackgsgofsr@gmail.com>
2023-11-24 01:49:13 -0300

    Changes to be committed: 	modified:   attention/mask.py 	new file:   quiz/artificial_intelligence.txt 	new file:   quiz/machine_learning.txt 	new file:   quiz/natural_language_processing.txt 	new file:   quiz/neural_network.txt 	new file:   quiz/probability.txt 	new file:   quiz/python.txt 	new file:   quiz/quiz0.png 	new file:   quiz/quiz1.png 	new file:   quiz/quiz2.png 	new file:   quiz/quiz3.png 	new file:   quiz/quiz4.png 	new file:   quiz/quiz5.png 	new file:   quiz/quiz6.png

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -4,11 +4,6 @@ import os
 import sys
 import tensorflow as tf
 
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import tensorflow as tf
-
-
 from sklearn.model_selection import train_test_split
 
 EPOCHS = 10
@@ -17,38 +12,35 @@ IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
 
+# Check command-line arguments
+if len(sys.argv) not in [2, 3]:
+    sys.exit("Usage: python traffic.py data_directory [model.h5]")
 
-def main():
+# Get image arrays and labels for all image files
+images, labels = load_data(sys.argv[1])
 
-    # Check command-line arguments
-    if len(sys.argv) not in [2, 3]:
-        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+# Split data into training and testing sets
+labels = tf.keras.utils.to_categorical(labels)
+x_train, x_test, y_train, y_test = train_test_split(
+    np.array(images), np.array(labels), test_size=TEST_SIZE
+)
 
-    # Get image arrays and labels for all image files
-    images, labels = load_data(sys.argv[1])
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    # Split data into training and testing sets
-    labels = tf.keras.utils.to_categorical(labels)
-    x_train, x_test, y_train, y_test = train_test_split(
-        np.array(images), np.array(labels), test_size=TEST_SIZE
-    )
+# Get a compiled neural network
+model = get_model()
 
-    x_train, x_test = x_train / 255.0, x_test / 255.0
+# Fit model on training data
+model.fit(x_train, y_train, epochs=EPOCHS)
 
-    # Get a compiled neural network
-    model = get_model()
+# Evaluate neural network performance
+model.evaluate(x_test,  y_test, verbose=2)
 
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
-
-    # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
-
-    # Save model to file
-    if len(sys.argv) == 3:
-        filename = sys.argv[2]
-        model.save(filename)
-        print(f"Model saved to {filename}.")
+# Save model to file
+if len(sys.argv) == 3:
+    filename = sys.argv[2]
+    model.save(filename)
+    print(f"Model saved to {filename}.")
 
 
 def load_data(data_dir):
@@ -124,8 +116,3 @@ def get_model():
     model.summary()
 
     return model
-
-
-if __name__ == "__main__":
-    main()
-    
